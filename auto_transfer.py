3e5067cca2848ee2542c6667849601ad43453475
«beho1der» <captain.perreiro@gmail.com>
2013-03-29 09:47:41 +0600

    add prefix to code city for corect work auto transfer

--- auto_transfer.py
+++ auto_transfer.py
@@ -12,6 +12,7 @@ class cdrbase:
 	DBUSER = "voip"
 	DBPASS = "voip2voip"
 	DBNAME = "asterisk"
+	PHONE_PREFIX = "351"
 
 
 	def db_init(self):
@@ -21,21 +22,26 @@ class cdrbase:
 		return connect 
 
 	def cdr(self,num): 
-		""" Функция поиска по cdr базе в asterisk """
+		""" Функция поиска по cdr базе в asterisk """ 
 		d = datetime.date.today()
 		date_start = d.strftime("%Y-%m-%d %H:%M:%S")
 		date_end = d.strftime("%Y-%m-%d 23:59:59")	
 		cursor = self.db_init().cursor()
-		cursor.execute("""SELECT src FROM cdr WHERE calldate>=(%s) AND calldate<=(%s) AND dst=(%s)  ORDER BY calldate DESC  LIMIT 1""",( date_start, date_end, num))
+		if (num[0:3] == self.PHONE_PREFIX):      # Нужно если городские номера отправляются к VOIP провайдеру без кода города,иначе закоментировать
+				num = num[3:]
+				num9 = '9' + num
+				cursor.execute("""SELECT src FROM cdr WHERE calldate>=(%s) AND calldate<=(%s) AND (dst=(%s) OR dst=(%s))   ORDER BY calldate DESC  LIMIT 1""",( date_start, date_end, num, num9))
+		else: 
+				num = '8' + num
+				num9 = '9' + num
+				cursor.execute("""SELECT src FROM cdr WHERE calldate>=(%s) AND calldate<=(%s) AND (dst=(%s) OR dst=(%s))   ORDER BY calldate DESC  LIMIT 1""",( date_start, date_end, num, num9))
 		if cursor.fetchone() != None:
 			for row in cursor:
 			    x = row[0] 
-		else:
-			    x='none' 
+		else: x='none' 
 		self.db_init().close()		
 		return x
-			
-
+	
 if __name__ == '__main__':
 	agi = AGI()
 	number = agi.env["agi_callerid"]
